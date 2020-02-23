499c369c0017f77abd9534af9911eddad853befc
kasungaya <kasunice.s@gmail.com>
2020-02-23 21:22:30 +0530

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Cats from '../views/Cats'
+import Dogs from '../views/Dogs'
+import Pet from '../views/Pet'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/cats',
+    name: 'cats',
+    component: Cats
+  },
+  {
+    path: '/dogs',
+    name: 'dog',
+    component: Dogs
+  },
+  {
+    path: '/pets/:species/:id',
+    name: 'pet',
+    component: Pet
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
